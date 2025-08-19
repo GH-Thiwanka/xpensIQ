c93417d612ccc2877fc981111cbebcb7b0b1969e
GH Thiwanka <infoshanpost@gmail.com>
2025-08-19 23:21:00 +0530

    add custom fonts and colors

---
+++ lib/constants/color.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Color kMainColor = Color(0xFF4CAF50);
+const Color kSecondaryColor = Color(0xFF388E3C);
+const Color kBgMaincolor = Color(0xFFC8E6C9);
+
+const Color kMainTextColor = Color(0xFF212121);
+const Color kSecondaryTextColor = Color(0xFF757575);
+const Color kBorDivColor = Color(0xFFE0E0E0);
+const Color kCardColor = Color(0xFFFFFFFF);
+
+const Color kBlueColor = Color(0xFF2196F3);
+const Color kAlertWarColor = Color(0xFFFF9800);
+const Color kExepenceColor = Color(0xFFF44336);
+const Color kSavingColor = Color(0xFF009688);
+
+const double kDefultPadding = 10;
